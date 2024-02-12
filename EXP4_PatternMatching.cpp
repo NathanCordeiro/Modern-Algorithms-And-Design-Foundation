134dc00218bd5b28ce93cbab4e9bda8af8391609
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2024-02-13 00:01:53 +0530

    Add files via upload

---
+++ EXP4_PatternMatching.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> match(string sentence, string pattern)
+{
+    vector<int> positions;
+    for (int i = 0; i < sentence.length() - pattern.length() + 1; i++)
+    {
+        bool matched = true;
+        for (int j = 0; j < pattern.length(); j++)
+        {
+            if (sentence[i + j] != pattern[j])
+            {
+                matched = false;
+                break;
+            }
+        }
+        if (matched)
+        {
+            positions.push_back(i);
+        }
+    }
+    return positions;
+}
+
+int main()
+{
+    string sentence, pattern;
+    cout << "Enter the string : ";
+    cin >> sentence;
+
+    cout << "Enter the pattern : ";
+    cin >> pattern;
+
+    vector<int> positions = match(sentence, pattern);
+
+    if (positions.empty())
+    {
+        cout << "Pattern not Present in the String";
+    }
+    else
+    {
+        cout << "Pattern found at indices : ";
+        for (int pos : positions)
+        {
+            cout << pos << " ";
+        }
+    }
+    return 0;
+}
